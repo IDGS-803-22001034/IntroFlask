454cb30a69df9a641851d0ff0e347c60490b9873
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-02-13 22:48:59 -0600

    Formularios

---
+++ forms.py
@@ -0,0 +1,11 @@
+from wtforms import form
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequierd,Email, EqualTo, Length, NumberRange, Regexp
+from wtforms import StringField, EmailField, PasswordField, SubmitField, IntegerField, SelectField, DecimalField
+
+class UserForm(Form):
+    matricula=StringField("Matricula")
+    edad=IntegerField("Edad")
+    nombre=StringField("Nombre")
+    apellidos=StringField("Apellidos")
+    email=EmailField("Correo")
